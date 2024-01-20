8152e2fa1dcd732f0cb2a9da62e8be69e93027e1
SWSJTX <iceNode@163.com>
2024-01-21 00:35:18 +0800

    feat: add check update function

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,30 @@
-export function setupCounter(element: HTMLButtonElement): void
+export interface checkOptions {
+  /**
+   * Initial request interval
+   */
+  init?: number,
+
+  /**
+   * Loop request interval
+   */
+  loop?: number,
+
+  /**
+   * Detects update callback functions
+   */
+  cb?: () => void,
+
+  /**
+   * Detection address URL
+   */
+  url?: string,
+
+  /**
+   * Cached Key
+   */
+  cacheKey?: string
+}
+
+export function clearTimer():void
+
+export function checkUpdate(options: checkOptions):void
